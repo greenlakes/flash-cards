75bf461141848c0db1acf020357d27b62712f046
Greenlakes <greenlakes2005@gmail.com>
2017-03-30 12:23:16 +0000

    finished task2

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,23 @@
+class CardsController < ApplicationController
+  def index
+    @cards = Card.all
+  end
+  
+  def show
+  end
+
+  def new
+  end
+  
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
