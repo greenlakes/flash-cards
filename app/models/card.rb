f38d390e56a0cfd922b0e711fa4cde5681a7ffeb
Greenlakes <greenlakes2005@gmail.com>
2017-04-01 09:36:48 +0000

    task 3 finish

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,17 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :not_the_same
+  
+  before_create :set_review_date, on: :create
+
+  def not_the_same
+    errors.add(:translated_text) if translated_text.downcase == original_text.downcase
+  end
+
+protected
+  
+  def set_review_date
+   self.review_date = Time.now + 3.days
+  end
+  
 end
