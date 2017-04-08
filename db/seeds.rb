68ddc9b644d19fe3f31696a422ad28d6821ff33a
Greenlakes <greenlakes2005@gmail.com>
2017-04-08 11:08:39 +0000

    before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'rubygems'
+require 'nokogiri'   
+require 'open-uri'
+page = Nokogiri::HTML(open("http://iloveenglish.ru/stories/view/basic_english_ch_2_850_samikh_neobkhodimikh_anglijskikh_slov"))   
+puts page.css('div#wrapper div#page.clearfix div.content-wrapper div.content div.story-content table tbody tr td').each do |el|
+  Card.create(original_text: el.first, translated_text: el.next_element)
+end
